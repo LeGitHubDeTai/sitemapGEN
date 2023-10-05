8fc447571178d05bf49888227f5c2d640c764cc0
LeGitHubDeTai <65470169+LeGitHubDeTai@users.noreply.github.com>
2023-10-05 13:13:33 +0200

    UPDATE

--- main.js
+++ main.js
@@ -30,7 +30,11 @@ if (siteOut.endsWith('/')) {
 
 // Fonction pour générer le sitemap.xml
 function generateSitemap() {
-    const sitemap = xmlbuilder.create('urlset', { encoding: 'UTF-8' });
+    const sitemap = xmlbuilder.create('urlset', { version: '1.0', encoding: 'UTF-8' })
+        .att('xmlns', 'http://www.sitemaps.org/schemas/sitemap/0.9')
+        .att('xmlns:xsi', 'http://www.w3.org/2001/XMLSchema-instance')
+        .att('xsi:schemaLocation', 'http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd');
+
 
     // Lire récursivement les fichiers du dossier spécifié
     function readDirectory(directory) {
